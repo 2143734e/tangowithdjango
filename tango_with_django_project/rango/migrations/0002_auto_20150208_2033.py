d5409e504b55e05d8887b0bc327e1545d75bf915
Lloyd Edgar Evangelista <2143734e@student.gla.ac.uk>
2015-02-09 00:04:54 +0000

    Chapter 8 - Completed

--- tango_with_django_project/rango/migrations/0002_auto_20150208_2033.py
+++ tango_with_django_project/rango/migrations/0002_auto_20150208_2033.py
@@ -19,6 +19,18 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='category',
+            name='slug',
+            field=models.SlugField(default=''),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='student_name',
+            field=models.IntegerField(default=2143734),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='category',
             name='views',
             field=models.IntegerField(default=0),
             preserve_default=True,
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,6 +2,8 @@ from rango.models import Category
 from rango.models import Page
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.forms import CategoryForm
+from rango.forms import PageForm
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -9,7 +11,8 @@ def index(request):
     # Retrieve the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary which will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    pages_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages' : pages_list}
 
     # Render the response and send it back!
     return render(request, 'rango/index.html', context_dict)
@@ -48,3 +51,54 @@ def category(request, category_name_slug):
 
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = CategoryForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render(request, 'rango/add_category.html', {'form': form})    
+
+def add_page(request, category_name_slug):
+
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+                cat = None
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                # probably better to use a redirect here.
+                return category(request, category_name_slug)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    context_dict = {'form':form, 'category': cat}
+
+    return render(request, 'rango/add_page.html', context_dict)
+    
